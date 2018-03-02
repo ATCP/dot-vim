1bcd0fae30dfdf4bd12c075029ff96985210db6d
ATP <poppin.ke@gmail.com>
2018-03-01 22:45:28 -0700

    add setup vncserver scripts

--- do.sh
+++ do.sh
@@ -1,5 +1,14 @@
+sudo apt install -y build-essential autoconf automake libxmu-dev
+
+sudo apt-get -y update
+sudo apt-get -y install vim htop
 
 cp -r .vim/ ~/ 
 cp -r .vimrc ~/
 
-sudo apt-get install ctags
+sudo apt-get -y install ctags
+
+sudo apt-get -y install vnc4server ubuntu-desktop
+sudo apt-get -y install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
+
+cp xstartup ~/.vnc/xstartup
