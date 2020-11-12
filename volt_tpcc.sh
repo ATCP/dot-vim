5c9d9674e2e05dbb0aa5dcab24af6baf0320b8f3
ke <poppin.ke@gmail.com>
2020-11-12 07:40:10 -0700

    ff

---
+++ volt_tpcc.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+cd ~/voltdb
+
+update-alternatives --config java
+
+export PATH="$PATH:$(pwd)/bin/"
+cd tests/test_apps/tpcc
+
+./run.sh server > server.log &
+
+./run.sh init
+./run.sh client 2>&1 | tee client.log
+
